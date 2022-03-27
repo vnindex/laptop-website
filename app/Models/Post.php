86e6e61cc7d8b2d8c40189af588ab7f5629c6500
quanghaui <2018603149@dhcnhn.edu.vn>
2022-03-27 10:22:33 +0700

    add relation models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,9 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'posts';
+    protected $fillable=['name','slug','description','content','keywords','image','status','view','category_id'];
 
     public function category()
     {
         return $this->belongsTo('App\PostCategory', 'category_id', 'id');
+    }
+
+    public static function getAllPost(){
+        return Post::with(['category'])->orderBy('id','DESC')->paginate(10);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,8 @@ class Product extends Model
 {
     protected $table = 'products';
 
+    protected $fillable=['name','quantity','sold','slug','category_id','brand_id','description','content','keywords','price','image','status'];
+
     public function brand()
     {
         return $this->belongsTo('App\Brand', 'brand_id', 'id');
@@ -24,4 +26,12 @@ class Product extends Model
         return $this->hasMany('App\OrderDetail', 'product_id', 'id');
     }
 
+    public static function getAllProduct(){
+        return Product::with(['category'])->orderBy('id','desc')->paginate(10);
+    }
+    
+    
+    public static function getProductBySlug($slug){
+        return Product::with(['category'])->where('slug',$slug)->first();
+    }
 }
