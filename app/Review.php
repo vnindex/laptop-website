1e1c89f9ee90eb80ee9e6f7391b29654d8c9263b
quanghaui <2018603149@dhcnhn.edu.vn>
2022-04-05 12:56:04 +0700

    create review model mapping

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $primaryKey = 'rev_id';
+
+    protected $fillable = [
+        'user_id',
+        'pro_id',
+        'comment',
+        'rate',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'pro_id');
+    }
 }
