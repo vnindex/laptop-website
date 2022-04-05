bcc4d79ffa208a5da578c0540888ae8c51404507
quanghaui <2018603149@dhcnhn.edu.vn>
2022-04-05 12:57:45 +0700

    create wishlist model mapping

--- app/Wishlist.php
+++ app/Wishlist.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wishlist extends Model
 {
-    //
+    protected $primaryKey = 'wish_id';
+
+    protected $fillable = [
+        'user_id',
+        'pro_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'pro_id');
+    }
 }
