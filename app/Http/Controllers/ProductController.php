4d31d9ecf91e89e50d42e0fe9da1c3b3ace5e037
quanghaui <2018603149@dhcnhn.edu.vn>
2022-04-05 17:40:12 +0700

    view to create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -38,7 +38,8 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('product.create', compact('categories'));
     }
 
     /**
