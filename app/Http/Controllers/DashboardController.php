00df0c0399cf56a9089844fbc535d5c0f07a7d9a
quanghaui <2018603149@dhcnhn.edu.vn>
2022-04-05 17:05:15 +0700

    add function to monitor in DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,82 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Review;
+use App\Product;
+use App\Order;
+use Carbon\Carbon;
+use App\Category;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+    public function index(){
+        $user_total = User::where('role', 0)->count();
+        $admin_total = User::where('role', 1)->count();
+        $user_male = User::where('gender', 1)->count();
+        $user_female = User::where('gender', 0)->count();
+        $user_buy = User::all();
+        $review_total = Review::all()->count();
+        
+        
+        // best product
+        $bestProducts = Product::orderBy('view', 'DESC')->limit(5)->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        //product follow price
+        $prices = Product::orderBy('pro_new_price', 'DESC')->limit(5)->get();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        
+        
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        return view('admin', compact(
+            'user_total',
+            'admin_total',
+            'user_male',
+            'user_female',
+            'review_total',
+            'user_buy',
+            'bestProducts',
+            'prices'
+        ));
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
