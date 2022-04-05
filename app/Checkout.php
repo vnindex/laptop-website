bfc5dbbd2ab0d7d38c5af5b2ac2d6245b7c600be
quanghaui <2018603149@dhcnhn.edu.vn>
2022-04-05 12:52:58 +0700

    create checkout model mapping

--- app/Checkout.php
+++ app/Checkout.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Checkout extends Model
 {
-    //
+    protected $primaryKey = 'check_id';
+    protected $fillable = [
+        'order_id',
+        'pro_id',
+        'quantity',
+        'total_price',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'pro_id');
+    }
 }
